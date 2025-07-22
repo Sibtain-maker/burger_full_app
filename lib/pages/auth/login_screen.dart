1920bbd9824ae76d15d040d3faa5db67bbf93be7
Sibtain <msibtain1236@gmail.com>
2025-07-22 18:15:02 +0500

    Authentication code

--- lib/pages/auth/login_screen.dart
+++ lib/pages/auth/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:burger_app_full/pages/auth/signup_screen.dart';
 import 'package:burger_app_full/widgets/my_button.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -16,64 +17,84 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.all(15),
-        child: Center(
-          child: Column(
-            children: [
-              Image.asset(
-                'assets/6343825.jpg',
-                width: double.maxFinite,
-                height: 500,
-              ),
-              // input text field for email
-              TextField(
-                controller: emailcontroller,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(15),
+          child: Center(
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/6343825.jpg',
+                  width: double.maxFinite,
+                  height: 500,
                 ),
-              ),
-              SizedBox(height: 20),
-              TextField(
-                controller: passwordcontroller,
-                decoration: InputDecoration(
-                  labelText: 'password',
-                  border: OutlineInputBorder(),
+                // input text field for email
+                TextField(
+                  controller: emailcontroller,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
-              SizedBox(height: 20),
-              SizedBox(
-                width: double.maxFinite,
-                child: MyButton(onPressed: () {}, buttonText: 'Login'),
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Dont have an account?', style: TextStyle(fontSize: 18)),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Signup_screen(),
-                        ),
-                      );
+                SizedBox(height: 20),
+                TextField(
+                  controller: passwordcontroller,
+                  decoration: InputDecoration(
+                    labelText: 'password',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20),
+                SizedBox(
+                  width: double.maxFinite,
+                  child: MyButton(
+                    onPressed: () async {
+                      final supbase = Supabase.instance.client;
+                      final AuthResponse res = await supbase.auth
+                          .signInWithPassword(
+                            email: emailcontroller.text,
+                            password: passwordcontroller.text,
+                          );
+                      if (res.user != null) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Login successful!')),
+                        );
+                      }
                     },
-                    child: Text(
-                      ' Sign Up',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: -1,
+                    buttonText: 'Login',
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Dont have an account?',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Signup_screen(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        ' Sign Up',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: -1,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/pages/auth/signup_screen.dart
+++ lib/pages/auth/signup_screen.dart
@@ -1,6 +1,7 @@
 import 'package:burger_app_full/pages/auth/login_screen.dart';
 import 'package:burger_app_full/widgets/my_button.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class Signup_screen extends StatefulWidget {
   const Signup_screen({super.key});
@@ -16,64 +17,82 @@ class _Signup_screenState extends State<Signup_screen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.all(15),
-        child: Center(
-          child: Column(
-            children: [
-              Image.asset(
-                'assets/login.jpg',
-                width: double.maxFinite,
-                height: 500,
-              ),
-              // input text field for email
-              TextField(
-                controller: emailcontroller,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(15),
+          child: Center(
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/login.jpg',
+                  width: double.maxFinite,
+                  height: 500,
                 ),
-              ),
-              SizedBox(height: 20),
-              TextField(
-                controller: passwordcontroller,
-                decoration: InputDecoration(
-                  labelText: 'password',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 20),
-              SizedBox(
-                child: MyButton(onPressed: () {}, buttonText: 'SIGN UP'),
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Already have an account?',
-                    style: TextStyle(fontSize: 18),
+                // input text field for email
+                TextField(
+                  controller: emailcontroller,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginScreen()),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  controller: passwordcontroller,
+                  decoration: InputDecoration(
+                    labelText: 'password',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20),
+                SizedBox(
+                  child: MyButton(
+                    onPressed: () async {
+                      final supbase = Supabase.instance.client;
+                      final AuthResponse res = await supbase.auth.signUp(
+                        email: emailcontroller.text,
+                        password: passwordcontroller.text,
                       );
+                      if (res.user != null) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Signup successful!')),
+                        );
+                      }
                     },
-                    child: Text(
-                      ' Login',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: -1,
+                    buttonText: 'SIGN UP',
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already have an account?',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginScreen(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        ' Login',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: -1,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
