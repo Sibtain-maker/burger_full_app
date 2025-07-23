f017448ee7bcce53c89b6b6ffaed7c9409bc0906
Sibtain <msibtain1236@gmail.com>
2025-07-24 00:44:38 +0500

    push this to main

--- lib/service/auth_service.dart
+++ lib/service/auth_service.dart
@@ -1,3 +1,5 @@
+import 'package:burger_app_full/pages/auth/login_screen.dart';
+import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AuthService {
@@ -12,8 +14,42 @@ class AuthService {
         return null;
       }
       return 'unknown Error occured';
+    } on AuthException catch (e) {
+      return e.message;
     } catch (e) {
-      throw Exception('Failed to sign up: $e');
+      return 'Error:$e';
+    }
+  }
+
+  // Login Function
+  Future<String?> Login_password(String email, String password) async {
+    try {
+      final response = await supabase.auth.signInWithPassword(
+        email: email,
+        password: password,
+      );
+      if (response.user != null) {
+        return null;
+      }
+      return 'Inavalid username or password';
+    } on AuthException catch (e) {
+      return e.message;
+    } catch (e) {
+      return 'Error:$e';
+    }
+  }
+
+  // Sign out function
+  Future<void> Logout(BuildContext context) async {
+    try {
+      await supabase.auth.signOut();
+      if (context.mounted) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const LoginScreen()),
+        );
+      }
+    } catch (e) {
+      print('Error signing out: $e');
     }
   }
 }
