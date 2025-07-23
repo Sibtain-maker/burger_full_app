8dd00d19ca52aad45a9691f9633e2bcf205bd37b
Sibtain <msibtain1236@gmail.com>
2025-07-23 22:54:26 +0500

    push the text to main

--- lib/pages/auth/signup_screen.dart
+++ lib/pages/auth/signup_screen.dart
@@ -46,6 +46,7 @@ class _Signup_screenState extends State<Signup_screen> {
                 ),
                 SizedBox(height: 20),
                 SizedBox(
+                  width: double.maxFinite,
                   child: MyButton(
                     onPressed: () async {
                       final supbase = Supabase.instance.client;
@@ -59,6 +60,7 @@ class _Signup_screenState extends State<Signup_screen> {
                         );
                       }
                     },
+
                     buttonText: 'SIGN UP',
                   ),
                 ),
